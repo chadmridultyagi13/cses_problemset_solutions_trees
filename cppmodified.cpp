985acd35c4b823d7f76a1f4efb379efce688fd59
MRIDUL TYAGI <mridultyagi@china.local>
2025-06-26 11:28:48 +0530

    Add cppmodified.cpp

---
+++ cppmodified.cpp
@@ -0,0 +1,209 @@
+// jai shree krishna
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠙⠒⠦⢄⣀⠀⠀⠀⠈⠙⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣉⣁⡒⠒⠢⠤⢄⣀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠶⣄⠀⠀⠀⠙⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⠶⠛⠉⠀⣀⣤⠈⢹⠆⠀⠀⠀⠀⠉⠓⠲⢤⣀⠀⠀⠀⠀⠀⠈⠙⢦⡀⠀⠈⢣⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⣴⠟⢁⡤⡾⢠⠞⠁⠘⠒⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠲⢤⡀⠀⠀⠀⠀⠙⣆⠀⠈⢧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⢀⡾⢡⡴⠋⢰⠃⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠳⣄⡀⠀⠀⠈⢧⡀⠸⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⣾⢁⡟⠀⠀⠸⡄⢸⠀⢀⡠⣤⠤⠤⠤⠤⠤⢤⣤⣄⣀⣀⠀⠀⠀⠀⠀⠀⠈⠙⣦⡀⠀⠀⢧⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⡏⢸⠀⠀⠀⠀⡇⢸⡶⠉⠀⠈⣧⠀⠀⠀⠀⠀⠀⠀⠈⠉⠙⠓⠶⢦⣄⡔⠉⢰⠈⠳⡄⠀⠘⣇⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⣷⠈⢧⡀⠀⣠⠇⣼⢧⣄⡀⠐⣧⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣤⣾⣿⢶⡞⠀⠀⢹⡆⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠘⣷⣄⣉⣉⣥⣞⡁⠀⠀⠙⠒⠛⠻⠿⢿⣶⣶⣶⣶⣶⣷⠿⠿⠛⠋⠁⠀⠀⠹⡆⠀⠀⢳⢠⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⢀⡏⠀⠀⠙⡉⠀⠉⢿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡄⠀⢸⡜⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠘⣷⣄⣀⣴⠁⠀⢤⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⡀⠀⠀⠀⠀⠀⢀⣠⡴⢷⠀⡜⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⢸⣹⠊⠁⠀⠀⣸⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠱⡀⠀⠹⡄⠀⠀⢀⡴⠟⠁⠀⠘⣷⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠹⣷⣄⣤⣴⠿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠱⡀⠀⢱⠀⡴⠋⠀⠀⠀⠀⣸⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⣦⠀⠀⠀⠀⠀⠀⢻⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠒⣥⢰⠁⠀⠀⠀⢀⡴⢷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⢻⣧⠀⠀⠀⣶⣤⣾⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣶⠿⠿⠛⠋⠀⠀⠀⠁⠘⡄⠀⠀⠋⢁⣠⣿⣞⣟⠤⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠈⠻⣷⣶⣾⡿⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⣠⠾⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⡄⢹⡏⠿⠛⠋⠸⡬⣷⡤⠤⠒⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠞⠁⢀⠤⠒⠋⠉⠉⠉⠂⣀⡀⠀⠀⠀⠀⠘⢦⠀⠀⠀⠀⠀⢻⣮⢧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⢀⣤⣶⡏⠁⠀⠀⠀⠀⠀⠀⠀⠳⣄⠀⠀⠀⠈⢷⣗⢕⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠫⣳⡲⣲⣾⢻⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢳⡀⠀⠀⢸⡷⣍⠁⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⣿⢾⡫⠄⠚⠁⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠙⢦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢄⡀⣀⣤⠤⠞⠁⠀⠀⠀⣿⣀⠉⠐⠒⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠘⡆⠀⣤⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⡀⠀⠀⣿⣌⠳⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠙⠶⣟⣞⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡤⠒⣊⠔⢻⠁⠀⠀⣿⢾⡕⠱⡀⠀⠀⠀⠀⠀⢀⡤⣾⣿⣿⡆⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⣾⠁⠀⢹⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠴⣯⣖⣉⣀⠴⠃⠀⠀⢸⢿⡿⣷⡄⠃⠀⠀⢀⣠⡾⠟⠀⠙⣿⣟⠁⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠈⣿⣦⣀⣼⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡿⠦⣫⡦⣉⠀⣠⡖⠋⠉⠀⣠⠴⠋⠀⢯⢆⢀⡀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠛⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⠇⠀⠘⣨⡵⠋⠉⠁⣀⡴⠊⠁⠀⠀⠀⢸⢠⠳⠛
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡟⣀⡴⠛⠛⠁⢀⡤⠞⠁⠀⠀⠀⠀⠀⠀⠈⠉⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠑⠲⠤⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⡿⢾⠇⠀⢀⣤⠞⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠙⠒⢲⡶⠦⠤⠴⠶⠖⢚⣿⠟⠉⠀⢀⣠⠖⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⢀⣠⠖⠛⠛⠀⣠⠴⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢧⣠⠴⠋⠀⠀⣀⡴⠚⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡴⠋⠁⠀⢀⡤⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠚⠁⠀⢀⣠⠞⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡠⠞⠁⠀⢀⣠⠔⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <ctime>
+#include <cassert>
+#include <climits>
+#include <cfloat>
+#include <vector>
+#include <list>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <bitset>
+#include <algorithm>
+#include <numeric>
+#include <utility>
+#include <functional>
+#include <sstream>
+#include <fstream>
+#include <random>
+#include <string>
+#include <iterator>
+#include <complex>
+#include <stdexcept>
+#include <memory>
+#include <new>
+#include <thread>
+#include <mutex>
+#include <future>
+#include <condition_variable>
+#include <tuple>
+#include <typeinfo>
+#include <limits>
+#include <array> 
+using namespace std;
+
+// Disjoint Set Union-Find
+class DisjointSet {
+    vector<int> rank, parent, size; 
+public: 
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0); 
+        parent.resize(n + 1);
+        size.resize(n + 1); 
+        for(int i = 0; i <= n; i++) {
+            parent[i] = i; 
+            size[i] = 1; 
+        }
+    }
+
+    int findUPar(int node) {
+        if(node == parent[node])
+            return node; 
+        return parent[node] = findUPar(parent[node]); 
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+        } else if(rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u; 
+        } else {
+            parent[ulp_v] = ulp_u; 
+            rank[ulp_u]++; 
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+            size[ulp_v] += size[ulp_u]; 
+        } else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v]; 
+        }
+    }
+}; 
+
+// Shortest Path using Dijkstra’s Algorithm
+vector<int> shortestPath(int n, int m, vector<vector<int> >& edges) {
+    vector<vector<pair<int, int> > > adj(n + 1);
+    for (size_t i = 0; i < edges.size(); i++) {
+        adj[edges[i][0]].push_back(make_pair(edges[i][1], edges[i][2]));
+        adj[edges[i][1]].push_back(make_pair(edges[i][0], edges[i][2]));
+    }
+    priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > qt;
+    vector<int> dist(n + 1, INT_MAX);
+    dist[1] = 0;
+    qt.push(make_pair(0, 1));
+    vector<int> par(n + 1, INT_MAX);
+    par[1] = -1;
+    
+    while (!qt.empty()) {
+        int node = qt.top().second;
+        int dis = qt.top().first;
+        qt.pop();
+        for (auto it : adj[node]) {
+            if (it.second + dist[node] < dist[it.first]) {
+                par[it.first] = node;
+                dist[it.first] = it.second + dist[node];
+                qt.push(make_pair(dist[it.first], it.first));
+            }
+        }
+    }
+    
+    vector<int> ans;
+    if (par[n] == INT_MAX) {
+        vector<int> result;
+        result.push_back(-1);
+        return result;
+    }
+    
+    int i = n;
+    while (par[i] != -1) {
+        ans.push_back(i);
+        i = par[i];
+    }
+    ans.push_back(1);
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+
+// Topological Sort (DFS)
+void dfs_topological(stack<int>& st, vector<int>& mark, vector<vector<int> >& adj, int node) {
+    mark[node] = 1;
+    for (auto it : adj[node]) {
+        if (mark[it] == 0) {
+            dfs_topological(st, mark, adj, it);
+        }
+    }
+    st.push(node);
+}
+
+// Topological Sort
+vector<int> topologicalSort(vector<vector<int> >& adj) {
+    stack<int> st;
+    vector<int> mark(adj.size(), 0);
+    for (size_t i = 0; i < adj.size(); i++) {
+        if (mark[i] == 0) {
+            dfs_topological(st, mark, adj, i);
+        }
+    }
+    vector<int> ans;
+    while (!st.empty()) {
+        int y = st.top();
+        st.pop();
+        ans.push_back(y);
+    }
+    return ans;
+}
+
+// Main Function
+int main() {
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+       
+
+    return 0;
+}}
